cfcce6a67dcdb381636d71c7a3d56674de30b63a
etorreborre <etorreborre@yahoo.com>
2023-09-08 15:18:36 +0200

    refactor(rust): move the controller address to the node manager
    so that it doesn't have to be passed in every call

--- implementations/rust/ockam/ockam_app/src/invitations/commands.rs
+++ implementations/rust/ockam/ockam_app/src/invitations/commands.rs
@@ -1,8 +1,9 @@
-use miette::IntoDiagnostic;
 use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::str::FromStr;
 use std::sync::Arc;
+
+use miette::IntoDiagnostic;
 use tauri::{AppHandle, Manager, Runtime, State};
 use tracing::{debug, info, trace, warn};
 
@@ -38,12 +39,7 @@ async fn accept_invitation_impl<R: Runtime>(id: String, app: &AppHandle<R>) -> c
 
     let node_manager_worker = state.node_manager_worker().await;
     let res = node_manager_worker
-        .accept_invitation(
-            &state.context(),
-            AcceptInvitation { id: id.clone() },
-            &state.controller_address(),
-            None,
-        )
+        .accept_invitation(&state.context(), AcceptInvitation { id }, None)
         .await?;
     debug!(?res);
     info!(?id, "Invitation accepted");
@@ -101,12 +97,7 @@ async fn send_invitation<R: Runtime>(
     let state: State<'_, AppState> = app.state();
     let node_manager_worker = state.node_manager_worker().await;
     let res = node_manager_worker
-        .create_service_invitation(
-            &state.context(),
-            invite_args,
-            &state.controller_address(),
-            None,
-        )
+        .create_service_invitation(&state.context(), invite_args, None)
         .await
         .map_err(|e| e.to_string());
     debug!(?res, "invitation sent");
@@ -128,7 +119,6 @@ pub async fn refresh_invitations<R: Runtime>(app: AppHandle<R>) -> Result<(), St
                 ListInvitations {
                     kind: InvitationListKind::All,
                 },
-                &state.controller_address(),
                 None,
             )
             .await
